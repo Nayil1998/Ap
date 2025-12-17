f4c7c0ed05dd752ccbbeda2da42ac2fad82615e7
NAYIL ALHRBI <naelx13@gmail.com>
2025-12-18 00:16:51 +0300

    Add user1522

--- Test.js
+++ Test.js
@@ -4,5 +4,11 @@
     "username": "user1176",
     "password": "pG!yd3wCw@",
     "expiry": "2026-01-16"
+  },
+  {
+    "Device Id": "6e4bef7b84220375",
+    "username": "user1522",
+    "password": "yHpb1BsyCK",
+    "expiry": "2026-01-16"
   }
 ]
