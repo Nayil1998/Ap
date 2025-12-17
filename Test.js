6306315ba2966ebcd0f51905bc69a7bfd23be414
NAYIL ALHRBI <naelx13@gmail.com>
2025-12-18 00:17:42 +0300

    Deleted user1522

--- Test.js
+++ Test.js
@@ -4,11 +4,5 @@
     "username": "user1176",
     "password": "pG!yd3wCw@",
     "expiry": "2026-01-16"
-  },
-  {
-    "Device Id": "6e4bef7b84220375",
-    "username": "user1522",
-    "password": "yHpb1BsyCK",
-    "expiry": "2026-01-16"
   }
 ]
