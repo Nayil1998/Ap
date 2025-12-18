1c409cbe590146fc02d2fa6680eecc539d43e5d2
NAYIL ALHRBI <naelx13@gmail.com>
2025-12-18 11:41:22 +0300

    Deleted user3767

--- Test.js
+++ Test.js
@@ -4,11 +4,5 @@
     "username": "user7134",
     "password": "GQbh3!x0we",
     "expiry": ""
-  },
-  {
-    "Device Id": "Sssßsßsss",
-    "username": "user3767",
-    "password": "R4kXw4cQyY",
-    "expiry": "2026-01-16"
   }
 ]
